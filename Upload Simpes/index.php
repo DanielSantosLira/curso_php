660e6cfe85d4a5384db1ba1fe40eef244ab01b8c
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-02-22 12:50:46 -0300

    Create index.php

---
+++ Upload Simpes/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload Simpes de Arquivo</title>
+</head>
+<body>
+    <div id="global">
+        <header>
+            <h1>Upload Simpes de Arquivo</h1>
+        </header>
+        <main>
+            <form    method="post" action="up_simples_melhorado.php"  enctype="multipart/form-data">
+                <label>Arquivo:
+                <input nome="arquivo_up" type="file" />
+                </label>
+                <button type="submit">Enviar</button>
+            </form>
+        </main>
+    </div>
+    
+</body>
+</html>
